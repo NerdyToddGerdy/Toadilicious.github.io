06b8383b614afe019540d446dd03ae74b64869a9
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-03-24 17:59:23 -0400

    Have a pretty good understanding of my pseudo code.

--- js/app.js
+++ js/app.js
@@ -11,3 +11,57 @@ console.log("so is jQuery");
 
 
 })
+
+
+
+
+
+//====================================================
+//                    PSEUDOCODE
+//====================================================
+
+/////////////Objects
+////Player 1
+  //Name
+  //Color
+  //Number of controlled spaces
+  //Number of soldiers
+
+////Player 2
+  //Name
+  //Color
+  //Number of controlled spaces
+  //Number of soldiers
+
+////Game
+  //has somebody won?
+  //attack phase
+    //Roll of Dice
+  //hire soldiers phase
+  //button
+  //Round Number
+
+////Map
+  // How to color change
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//====================================================
+//                END OF PSEUDOCODE
+//====================================================
