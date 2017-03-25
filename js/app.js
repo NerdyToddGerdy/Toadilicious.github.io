ae929f8a62844e6d69730f2839774e0f1562283c
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-03-24 21:53:32 -0400

    All of the buttons work.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,39 @@
 console.log("app.js is alive");
 $(function(){
 console.log("so is jQuery");
+//====================================================
+//                 GRAB ELEMENT
+//====================================================
+//Grab the spaces
+var $spaces = $('.space');
+var $button = $('.button');
 
 
+//====================================================
+//                EVENT HANDLER
+//====================================================
 
+//What happens when space is clicked
+var clickedSpace = function(){
+  console.log("test the click");
+}
 
+//What happens when the button is clickedSpace
+var clickedButton = function(){
+  console.log("test the button");
+}
 
+//====================================================
+//                EVENT LISTENER
+//====================================================
 
+//Turns the spaces into buttons
+$spaces.on('click', clickedSpace);
 
+//Turns the button Div into a button
+$button.on('click', clickedButton)
 
-
-})
+}); //End of Window onload
 
 
 
