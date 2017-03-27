9212141e89cbd6606680e47df36b898a509a2557
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-03-26 22:45:05 -0400

    Made very little progress on fixing the issues with my button pushing

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(function(){
   var $button = $('.button');
   var $player1ControlledSpaces = $('.player1');
   var $player2ControlledSpaces = $('.player2');
-
+  var counter = 0;
   // add controlled amount to player1
   player1.$numberOfControlledSpaces = $player1ControlledSpaces.length;
   // console.log(player1.$numberOfControlledSpaces);
@@ -25,19 +25,17 @@ $(function(){
     clickedSpace : function(){
       var $currentClass = $(this).attr('class');
       // console.log("check the class: " + $currentClass);
-      var thisLocation = $(this).index()
-      var upLocation = thisLocation-3;
-      var rightLocation = thisLocation+2;
-      var downLocation = thisLocation+5;
-      var leftLocation = thisLocation;
+      var $thisLocation = $(this).index()
+      var $upLocation = $thisLocation-4;
+      var $rightLocation = $thisLocation+1;
+      var $downLocation = $thisLocation+4;
+      var $leftLocation = $thisLocation-1;
 
-      var $upperCSS = $('#space-'+upLocation).css({'border': 'rgba(255,255,255,.7) solid 4px'});
-      var $rightCSS = $('#space-'+rightLocation).css({'border': 'rgba(255,255,255,.7) solid 4px'});
-      var $lowerCSS = $('#space-'+downLocation).css({'border': 'rgba(255,255,255,.7) solid 4px'});
-      var $leftCSS = $('#space-'+leftLocation).css({'border': 'rgba(255,255,255,.7) solid 4px'});
+      var $upperCSS = $('#space-'+$upLocation).css({'border': 'rgba(255,255,255,.7) solid 4px'});
+      var $rightCSS = $('#space-'+$rightLocation).css({'border': 'rgba(255,255,255,.7) solid 4px'});
+      var $lowerCSS = $('#space-'+$downLocation).css({'border': 'rgba(255,255,255,.7) solid 4px'});
+      var $leftCSS = $('#space-'+$leftLocation).css({'border': 'rgba(255,255,255,.7) solid 4px'});
       var $allSpaces = $('.space');
-
-      //Adds and removes the borders
 
       // UNCLICK STEP
       if ($(this).hasClass('clicked-space')) {
@@ -46,74 +44,94 @@ $(function(){
         $('.space').css({'border': 'rgba(255,255,255,0) solid 5px'})
         // console.log($(this).attr('class'));
 
-
       //CLICK STEP
       } else {
         $(this).removeClass('unclicked-space');
         $(this).addClass('clicked-space')
         $(this).css({'border': 'rgba(255,255,255,1) solid 5px'})
+
+        var $upperSelection = 'space-'+$upLocation;
+        var $rightSelection = 'space-'+$rightLocation;
+        var $downSelection = 'space-'+$downLocation;
+        var $leftSelection = 'space-'+$leftLocation;
+
+        console.log('I want to click these locations: ' + $upLocation + ', ' + $rightLocation + ', ' + $downLocation +', '+ $leftLocation);
         // console.log($(this).attr('class') + ' also');
+
       }
 
-
       //higlights nearby spaces.
-      if (thisLocation == 3) {
+      if ($thisLocation == 3) {
         // console.log("this is the top right corner box");
         $rightCSS.css({'border':'rgba(255,255,255,0'})
-      } else if (thisLocation === 4 || thisLocation ===8) {
+      } else if ($thisLocation === 4 || $thisLocation ===8) {
         // console.log("left side");
         $leftCSS.css({'border':'rgba(255,255,255,0'})
-      } else if (thisLocation === 7|| thisLocation === 11){
+      } else if ($thisLocation === 7|| $thisLocation === 11){
         // console.log("right side");
         $rightCSS.css({'border':'rgba(255,255,255,0'})
-      }else if (thisLocation === 12) {
+      }else if ($thisLocation === 12) {
         // console.log("bottom left");
         $leftCSS.css({'border':'rgba(255,255,255,0'})
       }
 
+      $('#' + $upperSelection).on('click',function(){
+        // $('.space').css({'border':'rgba(255,255,255,0'})
+        $(this).addClass('selected-border')    // How do I get this to light up when I just told it to go dark.
+        var $thisText = $(this).text()
+        console.log("this text = " + $thisText);
+        // game.rollOfDice();
+      });
+      $('#' + $rightSelection).on('click',function(){
+        console.log('this is location # ' + $rightSelection);
+      });
+      $('#' + $downSelection).on('click',function(){
+        console.log('this is location # ' + $downSelection);
+      });
+      $('#' + $leftSelection).on('click',function(){
+        console.log('this is location # ' + $leftSelection);
+      });
+      // counter++,
+      // console.log("counter: " + counter);
+    },
+    $currentId : $(this).attr('id'),
+    // console.log($currentId);
+    // console.log($thisLocation);
 
-
-    var $currentId = $(this).attr('id');
-    console.log($currentId);
-    console.log(thisLocation);
+    selectASpace:function(){
+      $('.space').css({'border':'rgba(255,255,255,0'})
+    }
 
 
     //select only the surrounding buttons
     // if this location is selected ID, make up, right, down, left available for selection.
     // if($)
-
-
-
-
-
-  },
+  // },
 
   //What happens when the button is clickedSpace
-  clickedButton : function(){
-    console.log("test the button");
-  }
+  // clickedButton:function(){
+  //   console.log("test the button");
+  // }
 }
 //====================================================
 //                EVENT LISTENER
 //====================================================
 
+
+
+  $spaces.on('click', eventHandler.clickedSpace);
+
+  // $spaces.on('click', eventHandler.selectASpace)
+
+//if counter is 0, run first click,
+//else run second click
 //Turns the spaces into buttons
-$spaces.on('click', eventHandler.clickedSpace);
 
 //Turns the button Div into a button
 $button.on('click', eventHandler.clickedButton)
 
 
-
-
-
-
-
-
 }); //End of Window onload**************************
-
-
-
 
 
 //====================================================
@@ -159,36 +177,55 @@ var game = {
   currentPlayer:player1,
   gameRound:0,
   currentPhase:'Attaack',
-  startOfGame: function(){
-    //Choose color
-    //choose name
-    this.attackPhase();
-  },
+  // startOfGame: function(){
+  //   player1.name: prompt('What is player 1\'s name?')
+  //
+  //   this.attackPhase();
+  // },
   checkForWinner: function(){
     //if all boxes are one color or the other.
+    if (player1.$numberOfControlledSpaces == 16){
+      alert('Congratulations ' + player1.name + "!!  You have won the game!")
+    } else if (player2.$numberOfControlledSpaces == 16){
+      alert('Congratulations ' + player2.name + "!!  You have won the game!")
+    } else {
+      this.attackPhase();
+    }
   },
   attackPhase: function(){
-    //need to select a space, see the surrounding spaces, compare dice to space.
+    //need to select a space, see the surrounding spaces,
+
+    //compare dice to space.
+    this.hirePhase();
   },
   rollOfDice: function(){
+
     console.log(Math.ceil(Math.random()*6));
     //sum of x random numbers where x = of soldiers in space
     //need the number of soldiers in each space.
   },
   hirePhase: function(){
-    // Calculate the number of spaces currently owned.
-    console.log(this.currentPlayer.$numberOfControlledSpaces);
-    // make newsoldiers = spaces owned.
-    // add a new soldier to clicked player's spaces
-    // subtract 1 from newsoldiers
+  //   // Calculate the number of spaces currently owned.
+  //   console.log(this.currentPlayer.$numberOfControlledSpaces);
+  //   // make newsoldiers = spaces owned.
+  //   var newSoldiers = this.currentPlayer.$numberOfControlledSpaces;
+  //   console.log(newSoldiers);
+  //   while (newSoldiers > 0){// add a new soldier to clicked player's space
+      // if (/*selectedSpace*/.hasClass(this.currentPlayer){
+      //   /*selectedSpace*/++;
+      //   newSoldiers--;
+      // }
+  //   newSoldiers--;
+  // }
+  //   // when newSoldiers is 0, go to game.endOfRound
   },
   endOfRound: function(){
+    this.checkForWinner();
     if (this.currentPlayer === player1){
       this.currentPlayer = player2;
     } else {
       this.currentPlayer = player1;
     }
-    this.checkForWinner();
   }
 }
 ////Map
