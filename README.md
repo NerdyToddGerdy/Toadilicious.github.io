5e1ef82d82cab1694c726ad6f33c5c0932dc0d8f
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-03-30 11:19:05 -0400

    testing the png images on github

---
+++ README.md
@@ -0,0 +1,21 @@
+ technologies used, the approach taken, a link to your live site, installation instructions, unsolved problems,
+
+Shows off all features of the app
+Explains the technical details
+Explains the technical challenges
+Explains which improvements you might make
+
+#Island Pillaging
+##Brief description
+
+##technologies used
+
+##the approach taken
+
+##link to live site
+
+##installation instructions
+
+##unsolved problems
+
+##expected updates
--- js/app.js
+++ js/app.js
@@ -24,9 +24,8 @@ $(function(){
   //====================================================
   //                EVENT HANDLER
   //====================================================
+
   var eventHandler = {
-    $currentClass : $(this).attr('class'),
-    // console.log("check the class: " + $currentClass);
     resetSpace: function(){
       console.log('HERE BE DRAGONS!!!');
       game.resetting();
@@ -38,9 +37,9 @@ $(function(){
       clickedText2= $(this).text();
       console.log('#2: ' + clickedNumber2);
       game.attackPhase();
+      eventHandler.resetSpace();
     },
     clickedSpace : function(){
-      //more to come.
 
 
       //******This Section finds the index of the 4 surrounding spaces.
@@ -64,42 +63,48 @@ $(function(){
         $('#space-' + rightLocation).not($('.' + game.currentPlayer)).addClass('new-space');
       };
       var addBorderDown =function(){
-         $('#space-' + downLocation).not($('.' + game.currentPlayer)).addClass('new-space');
+        $('#space-' + downLocation).not($('.' + game.currentPlayer)).addClass('new-space');
       };
       var addBorderLeft =function(){
-         $('#space-' + leftLocation).not($('.' + game.currentPlayer)).addClass('new-space');
+        $('#space-' + leftLocation).not($('.' + game.currentPlayer)).addClass('new-space');
       };
 
 
       //This statement fixes the side from looking at the far sides of the grid.
-if ($(this).text() > 1) {
-  console.log('ok');
-      if (thisLocation === 3) {
-        addBorderDown();
-        addBorderLeft();
-      } else if (thisLocation === 4 || thisLocation === 8) {
-        addBorderDown();
-        addBorderRight();
-        addBorderUp();
-      } else if (thisLocation === 7 || thisLocation === 11) {
-        addBorderUp();
-        addBorderLeft();
-        addBorderDown();
-      } else if (thisLocation === 12) {
-        addBorderUp();
-        addBorderRight();
+      if ($(this).text() > 1) {
+        console.log('ok');
+        if (thisLocation === 3) {
+          addBorderDown();
+          addBorderLeft();
+        } else if (thisLocation === 4 || thisLocation === 8) {
+          addBorderDown();
+          addBorderUp();
+          addBorderRight();
+        } else if (thisLocation === 7 || thisLocation === 11) {
+          addBorderUp();
+          addBorderLeft();
+          addBorderDown();
+        } else if (thisLocation === 12) {
+          addBorderUp();
+          addBorderRight();
+        } else {
+          addBorderUp();
+          addBorderRight();
+          addBorderDown();
+          addBorderLeft();
+        }
+        $(this).addClass('clicked-space');
       } else {
-        addBorderUp();
-        addBorderRight();
-        addBorderDown();
-        addBorderLeft();
+        // swal({
+        //   target:'.container',
+        //   text:'Oops...'
+        //   // 'Something went wrong!',
+        //   // 'error'
+        // });
+        alert('You do not have enough soldiers here to attack. Please pick another island or end the phase.');
+        // $('.space').off(); //removed button from all spaces
+        game.resetting(); //clears everything
       }
-      $(this).addClass('clicked-space');
-    } else {
-      alert('You do not have enough soldiers here to attack. Please pick another island or end the phase.');
-      // $('.space').off(); //removed button from all spaces
-      game.resetting(); //clears everything
-    }
 
       // $(this).addClass('clicked-space');
       $('.space').off(); //removed button from all spaces
@@ -113,57 +118,45 @@ if ($(this).text() > 1) {
       $('#space-' + downLocation).not($('.' + game.currentPlayer)).on('click', eventHandler.newSpace);
       $('#space-' + leftLocation).not($('.' + game.currentPlayer)).on('click', eventHandler.newSpace);
 
-
-
-
-      // $('.space').not($(this)).on('click', eventHandler.newSpace);//spaces that are not my current space are options to attack
       console.log(clickedText1);
       console.log('#1: ' + clickedNumber1);
 
     },
     $currentId : $(this).attr('id'),
   };
-test = eventHandler.clickedSpace;
-//====================================================
-//                EVENT LISTENER
-//====================================================
+  test = eventHandler.clickedSpace;
+  //====================================================
+  //                EVENT LISTENER
+  //====================================================
 
-var testing = function(){
-  var $player1Score = $('.player1').length;
-  var $player2Score = $('.player2').length;
-  $('.scores').html('<p>Player 1\'s points: ' + $player1Score + '</p> <p>Player 2\'s points: ' + $player2Score + '</p>');
-  $('.rules').html('<h3>Attack Phase</h3><p>By selecting any of your spaces that have at least 2 points in it, you can attack any space touching yours horizontally or vertically. Repeat until you have no more possible moves.  When done, press \'End Phase\' to continue.</p>');
+  var testing = function(){
+    var $player1Score = $('.player1').length;
+    var $player2Score = $('.player2').length;
+    $('.scores').html('<p>Player 1\'s points: ' + $player1Score + '</p> <p>Player 2\'s points: ' + $player2Score + '</p>');
+    $('.rules').html('<h3>Attack Phase</h3><p>By selecting any of your spaces that have at least 2 points in it, you can attack any space touching yours horizontally or vertically. Repeat until you have no more possible moves.  When done, press \'End Phase\' to continue.</p>');
 
-  // $('.phase').text('Attack Phase');
-  if (toggle === true) {
-    game.currentPlayer = 'player1';
-    otherPlayer = 'player2';
-    $('.turns').html('<p>Player 1\'s turn</p><p class="phase">Attacking Phase</p>').css({'box-shadow':'inset 0 0 0 10px slategrey'});
-    console.log(game.currentPlayer);
-  }else{
-    game.currentPlayer = 'player2';
-    otherPlayer = 'player1';
-    $('.turns').html('<p>Player 2\'s turn</p><p class="phase"><p>Attacking Phase</p>').css({'box-shadow':'inset 0 0 0 10px darkgoldenrod'});
-    console.log(game.currentPlayer);
-  }
-  $('.'+game.currentPlayer).on('click', eventHandler.clickedSpace);
-};
-  // $spaces.on('click', eventHandler.selectASpace)
-//if counter is 0, run first click,
-//else run second click
-//Turns the spaces into buttons
-//Turns the button Div into a button
-$button.on('click', eventHandler.clickedButton);
+    // $('.phase').text('Attack Phase');
+    if (toggle === true) {
+      game.currentPlayer = 'player1';
+      otherPlayer = 'player2';
+      $('.turns').html('<p>Player 1\'s turn</p><p class="phase">Attacking Phase</p>').css({'box-shadow':'inset 0 0 0 10px slategrey'});
+      console.log(game.currentPlayer);
+    }else{
+      game.currentPlayer = 'player2';
+      otherPlayer = 'player1';
+      $('.turns').html('<p>Player 2\'s turn</p><p class="phase"><p>Attacking Phase</p>').css({'box-shadow':'inset 0 0 0 10px darkgoldenrod'});
+      console.log(game.currentPlayer);
+    }
+    $('.'+game.currentPlayer).on('click', eventHandler.clickedSpace);
+  };
 
-begin = testing;
-clickedSpace = eventHandler.clickedSpace;
-testing();
+  $button.on('click', eventHandler.clickedButton);
+
+  begin = testing;
+  clickedSpace = eventHandler.clickedSpace;
+  testing();
 }); //End of Window onload**************************
 
-
-//====================================================
-//                    PSEUDOCODE
-//====================================================
 
 /////////////Objects
 var clickedSpace;
@@ -177,11 +170,8 @@ var test;
 var testing;
 var begin;
 var toggle = true;
-////Player 1
-//Name
-//Color
-//Number of controlled spaces
-//Number of soldiers
+
+
 var player1 = {
   name:'',
   playerColor:'',
@@ -190,11 +180,6 @@ var player1 = {
   currnetlyContolledSpaces:[]
 };
 
-////Player 2
-//Name
-//Color
-//Number of controlled spaces
-//Number of soldiers
 var player2 = {
   name:'',
   playerColor:'',
@@ -202,13 +187,7 @@ var player2 = {
   numberOfSoldiers:2,
   currnetlyContolledSpaces:[]
 };
-////Game
-//has somebody won?
-//attack phase
-//Roll of Dice
-//hire soldiers phase
-//button
-//Round Number
+
 var game = {
   currentPlayer:'player1',
   // currentPlayerString:
@@ -218,6 +197,8 @@ var game = {
 
   checkForWinner: function(){
     console.log('CHECKING FOR WINNER');
+    player1.$numberOfControlledSpaces = $('.player1').length;
+    player1.$numberOfControlledSpaces = $('.player1').length;
     //if all boxes are one color or the other.
     if (player1.$numberOfControlledSpaces == 16){
       alert('Congratulations ' + player1.name + "!!  You have won the game!");
@@ -230,15 +211,14 @@ var game = {
   },
 
 
-resetting: function(){
-  $('.space').removeClass('clicked-space').removeClass('new-space');
-  console.log('clicked-space and new-space classRemoved');
-  $('.space').off();
-  // $('.space').on('click',test);///////////////////////////////////////////////////////////////////
-  clickedNumber1 = '';
-  clickedNumber2 = '';
-  $('.' + game.currentPlayer).on('click',clickedSpace);
-},
+  resetting: function(){
+    $('.space').removeClass('clicked-space').removeClass('new-space');
+    console.log('clicked-space and new-space classRemoved');
+    $('.space').off('click');
+    clickedNumber1 = '';
+    clickedNumber2 = '';
+    $('.' + game.currentPlayer).on('click',clickedSpace);
+  },
 
 
   claimLand: function(){
@@ -268,7 +248,6 @@ resetting: function(){
       // console.log(clickedText2 + "this is the second clicked");
       game.rollOfDice();
     }
-
     game.resetting();
   },
 
@@ -286,28 +265,27 @@ resetting: function(){
       // console.log('doing the attack roll');
     }
     // console.log("Attacker Roll " +parseInt(attackerTotal));
-      for (var j = 0; j < parseInt(clickedText2); j++) {
-        defenderTotal = defenderTotal + sixSidedDie();
+    for (var j = 0; j < parseInt(clickedText2); j++) {
+      defenderTotal = defenderTotal + sixSidedDie();
     }
     // console.log('Defender Roll ' + defenderTotal);
     if (attackerTotal > defenderTotal) {
       // console.log('Attacker Wins');
-    game.claimLand();
-  }else {
-    $('#space-' + clickedNumber1).html('<h2>'+1+'</h2>');
-  }
+      game.claimLand();
+    }else {
+      $('#space-' + clickedNumber1).html('<h2>'+1+'</h2>');
+    }
     // console.log('defender wins');
   },////Running to my specifications!!!
 
 
   endOfRound: function(){
     // console.log('END OF ROUND  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-    game.checkForWinner();
     $('.new-space').removeClass('new-space');
     $('.clicked-space').removeClass('clicked-space');
 
-      // console.log(clickedText1);
-      // console.log('#1: ' + clickedNumber1);
+    // console.log(clickedText1);
+    // console.log('#1: ' + clickedNumber1);
     if (toggle === true) {
       toggle = false;
       // console.log('true >> false');
@@ -316,12 +294,15 @@ resetting: function(){
       // console.log('false >> true');
     }
     // console.log("Toggle = " + toggle);
-      begin();
+    begin();
   },
 
 
 
   hirePhase: function(){
+    game.checkForWinner();
+
+    $(".button").animate({left: '250px'});
     //Comes from attackPhase
     $('.turns').html('<p>' + game.currentPlayer +'\'s turn</p><p class="phase"><p>Rebuild Phase</p>');
     var curentPlayerClass = '.' + game.currentPlayer;
@@ -334,7 +315,7 @@ resetting: function(){
 
     if (game.currentPlayer === 'player1') {
       player1.$numberOfControlledSpaces = $('.' + game.currentPlayer).length;
-     newSoldiers = player1.$numberOfControlledSpaces;
+      newSoldiers = player1.$numberOfControlledSpaces;
       console.log('player1 gets to hire now');
 
     } else {
@@ -342,7 +323,7 @@ resetting: function(){
       console.log($('.' + game.currentPlayer).length + ' = currentPlayer length');
       player2.$numberOfControlledSpaces = ($('.' + game.currentPlayer).length);
       console.log($('.' + game.currentPlayer).length);
-     newSoldiers = player2.$numberOfControlledSpaces;
+      newSoldiers = player2.$numberOfControlledSpaces;
       console.log('player2 gets to hire now');
     }
 
@@ -367,25 +348,3 @@ resetting: function(){
     $('.button').on('click', game.endOfRound);
   },
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//====================================================
-//                END OF PSEUDOCODE
-//====================================================
